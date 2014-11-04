c01848563023f1fe662c25a7dd65c04da580ada2
sangtrants <sangtrants@outlook.com>
2014-11-04 13:36:25 +0700

    CSDL va template

---
+++ QuanLyThuVien-N8/QuanLyThuVien-N8/Controllers/ThuThuController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyThuVien_N8.Controllers
+{
+    public class ThuThuController : Controller
+    {
+        //
+        // GET: /ThuThu/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
